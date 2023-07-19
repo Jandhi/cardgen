1f2c93a7fe66477b3718a2964e0377741a3603ba
Jan D <jandohring@hotmail.com>
2023-07-19 00:45:26 -0400

    dice icons

--- icon_atlas.py
+++ icon_atlas.py
@@ -1,4 +1,12 @@
 icon_paths = {
     'dmg' : 'Damage.png',
     'damage' : 'Damage.png',
+    
+    # DICE
+    'dice1' : 'dice/1.png',
+    'dice2' : 'dice/2.png',
+    'dice3' : 'dice/3.png',
+    'dice4' : 'dice/4.png',
+    'dice5' : 'dice/5.png',
+    'dice6' : 'dice/6.png',
 }
---
+++ location_cards.py
@@ -0,0 +1,62 @@
+from core.schema import Schema
+from data.source import OnlineSource
+from elements.text import TextElement
+from elements.rect import RectElement
+from elements.conditional import ConditionalElement
+from core.alignment import TopCenter, MiddleCenter
+from core.geometry import Point
+from core.scaling import PARENT
+from core.color import Color
+
+
+src = OnlineSource('https://docs.google.com/spreadsheets/d/e/2PACX-1vQd8e0poc7VR1-vKW3GnrVuywCL0IHOHAMfUpW3m90ctsUOgClQL04NuzVUNid8Q5Cb9PwjGT5hXPdt/pub?gid=0&single=true&output=csv')
+schema = Schema(
+    naming='locations/$name$',
+    elements=[
+        # Title
+        TextElement(
+            text='$name$', 
+            font_path='squealer', 
+            fill='blue',
+            font_size=60,
+            offset=Point(0, 50),
+            alignment=TopCenter
+        ),
+
+        # Underline
+        RectElement(
+            fill='blue',
+            alignment=TopCenter,
+            size=Point(PARENT - 80, 1),
+            offset=Point(0, 120)
+        ),
+
+        # Domains
+        TextElement(
+            text='$domains$',
+            font_path='alegreya_italic_bold',
+            font_size=40,
+            fill='black',
+            alignment=TopCenter,
+            offset=Point(0, 120),
+        ),
+
+        # Effect
+        RectElement(
+            fill='white',
+            alignment=TopCenter,
+            size=Point(PARENT - 80, PARENT - 150),
+            offset=Point(0, 170),
+            children=[
+                TextElement(
+                    text='$effect$',
+                    font_path='alegreya',
+                    fill='black',
+                    font_size=40,
+                    alignment=MiddleCenter
+                )
+            ]
+        )
+    ]
+)
+schema.process(src)
